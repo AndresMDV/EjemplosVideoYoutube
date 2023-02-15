c7bdcb777d12e5216357c6b6e709975af3f1e25f
Andres_Medina <andalfmedinag@hotmail.com>
2023-02-14 19:26:27 -0500

    Final - Video

--- Hola-mundo/dist/index.js
+++ Hola-mundo/dist/index.js
@@ -4,4 +4,40 @@ mensaje = "chanchito feliz";
 mensaje = "chao mundo";
 console.log(mensaje);
 console.log(typeof []);
+let extincionDinosaurios = 76000000;
+let dinosaurioFavorito = "T-Rex";
+let extintos = true;
+function chanchitoFeliz(config) {
+    return config;
+}
+let animales = ['chanchito', 'feliz', 'felipe'];
+let nums = [1, 2, 3];
+let checks = [];
+let nums2 = [];
+let tupla = [1, animales];
+const chica = 's';
+const mediana = 'm';
+const grande = 'l';
+const extragrande = 'xl';
+var Talla;
+(function (Talla) {
+    Talla["Chica"] = "s";
+    Talla["Mediana"] = "m";
+    Talla["Grande"] = "l";
+    Talla["ExtraGrande"] = "xl";
+})(Talla || (Talla = {}));
+const variabel1 = Talla.Grande;
+console.log(variabel1);
+const estado = 2;
+const objeto = {
+    id: 1,
+    nombre: 'Hola Mundo',
+    talla: Talla.Chica,
+    direccion: {
+        numero: 1,
+        calle: 'Siempre Vacio',
+        pais: 'Chanchitolandia'
+    }
+};
+const arr = [];
 //# sourceMappingURL=index.js.map
